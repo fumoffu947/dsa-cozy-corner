a846b276ada816011c727b0bd5c8e2246ef79ac5
Philip Johansson <philip_yeah@hotmail.com>
2017-04-13 20:33:53 +0200

    Add a header file for graph search.

---
+++ src/graph_search.h
@@ -0,0 +1,8 @@
+#ifndef GRAPH_SEARCH
+#define GRAPH_SEARCH
+#include "graph_node.h"
+
+GraphNode<std::pair<float, float>>* a_star_search(GraphNode<std::pair<float, float>>* start_node, std::pair<float, float> end_point, void (*heuristic)(std::pair<float, float>, std::pair<float, float>));
+
+#endif // !GRAPH_SEARCH
+
