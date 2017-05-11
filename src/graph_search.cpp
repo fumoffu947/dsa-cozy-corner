30570634af0e3e1b9328b3509e0e3d2582c5e2f6
Philip Johansson <philip_yeah@hotmail.com>
2017-05-11 20:49:45 +0200

    Add small changes to the graph_search.

--- src/graph_search.cpp
+++ src/graph_search.cpp
@@ -1,6 +1,7 @@
 #include "graph_node.h"
+#include <vector>
 #include <unordered_set>
 
-GraphNode<std::pair<float, float>>* a_star_search(GraphNode<std::pair<float, float>>* start_node, std::pair<float, float> end_point, void(*heuristic)(std::pair<float, float>, std::pair<float, float>)) {
+vector<GraphNode<std::pair<float, float>>*> heuristic_search(GraphNode<std::pair<float, float>>* start_node, std::pair<float, float> end_point, void(*heuristic)(std::pair<float, float>, std::pair<float, float>)) {
 	std::unordered_set<GraphNode<std::pair<float, float>>*> explored_nodes;
 }
--- src/graph_search.h
+++ src/graph_search.h
@@ -2,7 +2,7 @@
 #define GRAPH_SEARCH
 #include "graph_node.h"
 
-GraphNode<std::pair<float, float>>* a_star_search(GraphNode<std::pair<float, float>>* start_node, std::pair<float, float> end_point, void (*heuristic)(std::pair<float, float>, std::pair<float, float>));
+GraphNode<std::pair<float, float>>* heuristic_search(GraphNode<std::pair<float, float>>* start_node, std::pair<float, float> end_point, void (*heuristic)(std::pair<float, float>, std::pair<float, float>));
 
 #endif // !GRAPH_SEARCH
 
